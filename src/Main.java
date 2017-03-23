01cf96ce2fb5bc7327cf7a28c432ff0757a34e62
Autliety <whitefryfish@gmail.com>
2017-03-24 00:15:34 +0800

    v1.0 finished

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+/**
+ * @Author: Autliety
+ * @date: 2017/3/20
+ */
+public class Main {
+  public static ArrayList listPro = new ArrayList();
+
+  public static void main(String[] args) {
+    new GUI();
+  }
+}
